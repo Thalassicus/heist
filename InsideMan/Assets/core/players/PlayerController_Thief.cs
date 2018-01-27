881e9bd01cb7c44902340c73d57b1686bc5f56c5
James Isbell <jisbell01@gmail.com>
2018-01-27 11:26:46 -0600

    First!

---
+++ InsideMan/Assets/core/players/PlayerController_Thief.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_Thief : MonoBehaviour {
+	float moveSpeed = 0.1f;
+
+	void Update () { 
+		var moveVector = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		moveVector.Normalize();
+		moveVector = moveVector * moveSpeed;
+		transform.
+		transform.Translate(moveVector.x, moveVector.y, 0);
+	}
+}
