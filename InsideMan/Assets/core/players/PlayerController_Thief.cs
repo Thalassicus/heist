a41aae7f0ceea589502df1f59be510f9f7d6a41c
James Isbell <jisbell01@gmail.com>
2018-01-27 13:19:58 -0600

    b2
    Thief can pick up objective and bring it to the exit

--- InsideMan/Assets/core/players/PlayerController_Thief.cs
+++ InsideMan/Assets/core/players/PlayerController_Thief.cs
@@ -3,18 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController_Thief : MonoBehaviour {
-	float moveSpeed = 0.1f;
-	Rigidbody2D rigidbody2D = new Rigidbody2D();
+	public float moveSpeed = 1f;
+	public float sprintSpeed = 2f;
+	public float carryMultiplier = 0.8f;
+	bool isSprinting = false;
+	float moveSpeedActual = 0f;
+	float sprintSpeedActual = 0f;
+
+	bool hasObjective = false;
+
+	//Rigidbody2D rigidbody2D = new Rigidbody2D();
 
 	void Start(){
-		rigidbody2D = ((Rigidbody2D)GetComponent(typeof(Rigidbody2D)));
+		//rigidbody2D = ((Rigidbody2D)GetComponent(typeof(Rigidbody2D)));
+		moveSpeedActual = moveSpeed/1000;
+		sprintSpeedActual = sprintSpeed/1000;
 	}
 
-	void Update () { 
+	void Update () {
 		var moveVector = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		var speed = moveSpeedActual;
 		moveVector.Normalize();
-		moveVector = moveVector * moveSpeed;
+		isSprinting = Input.GetAxis ("Horizontal") != 0;
+		if(isSprinting){
+			speed = sprintSpeedActual;
+		}
+		if(hasObjective){
+			speed = speed * carryMultiplier;
+		}
+		moveVector = moveVector * speed;
 		transform.Translate (moveVector);
 		//rigidbody2D.MovePosition(rigidbody2D.position + moveVector);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log("Triggered!");
+		if( collider.tag == "Objective" ){
+			hasObjective = true;
+			collider.gameObject.transform.SetParent(transform);
+			collider.gameObject.transform.localPosition = new Vector3 (0f, 0.1f, 0f);
+			Debug.Log("Picked up the objective!");
+		}
+		if(collider.tag == "Exit" && hasObjective){
+			// End Game
+			Debug.Log("WOOOOOO!!!!");
+		}
 	}
 }
