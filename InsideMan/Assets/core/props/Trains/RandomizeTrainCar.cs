0ecb9768349e6796f598ed92c2a295d8ca9c06dd
Victor <Thalassicus@hotmail.com>
2018-01-27 13:32:35 -0600

    Level design

---
+++ InsideMan/Assets/core/props/Trains/RandomizeTrainCar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeTrainCar : MonoBehaviour {
+	private string spriteNames = "snk-sprt-traincar";
+	private int spriteVersion = 0;
+	private SpriteRenderer spriteR;
+	private Sprite[] sprites;
+	private BoxCollider2D collider;
+
+	void Start()
+	{
+		spriteR = gameObject.GetComponent<SpriteRenderer>();
+		collider = gameObject.GetComponent<BoxCollider2D>();
+		sprites = Resources.LoadAll<Sprite>(spriteNames);
+
+		if (Random.Range (0f, 1f) < 0.4) {
+			// boxcar
+			spriteR.sprite = sprites [0];
+			collider.enabled = false;
+		} else {
+			// tanker
+			spriteR.sprite = sprites [1];
+			collider.enabled = true;
+		}
+
+		spriteR.flipX = Random.Range (0f, 1f) > 0.5;
+	}
+}
