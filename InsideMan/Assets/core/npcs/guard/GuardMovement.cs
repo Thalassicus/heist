cd224601c3b7471729126eb0df9713bae8a0872e
Trostka <kyletrost@verizon.net>
2018-02-09 10:31:34 -0600

    Collision Delay and Wait Coroutines for Guard
    After a short delay upon colliding with a patrol point, the guard will
    wait a few seconds before moving to the next point along its path (the
    first delay makes sure the guard is on top of the patrol point, which
    makes designing the map more intuitive)

--- InsideMan/Assets/core/npcs/guard/GuardMovement.cs
+++ InsideMan/Assets/core/npcs/guard/GuardMovement.cs
@@ -7,6 +7,8 @@ public class GuardMovement : MonoBehaviour
     // public variables
     public GameObject[] patrolPoints;   // List of the guard's patrol points
     public float moveSpeed;             // The guard's movement speed
+    public float collisionDelay;        // Delay after the guard collides with the patrol point
+    public float waitTime;              // Time the guard waits before proceeding to the next patrol point
 
     // private variables
     GameObject currentPoint;    // The point that the guard is currently moving towards
@@ -34,16 +36,34 @@ public class GuardMovement : MonoBehaviour
     {
         if(other.tag == "Patrol_Point")
         {
-            if(currentPointIndex < patrolPoints.Length - 1)
-            {
-                currentPoint = patrolPoints[++currentPointIndex];
-                Debug.Log(currentPointIndex);
-            }
-            else
-            {
-                currentPoint = patrolPoints[0];
-                currentPointIndex = 0;
-            }
+            StartCoroutine(DelayAfterColliding(collisionDelay));
+        }
+    }
+
+    // Short delay after colliding with patrol point
+    IEnumerator DelayAfterColliding(float collDelay)
+    {
+        // Get ready to move to the next point after collDelay seconds
+        yield return new WaitForSeconds(collDelay);
+
+        StartCoroutine(WaitBeforePatrolling(waitTime));
+    }
+
+    // Wait for waitTime seconds, then get the next patrol point
+    IEnumerator WaitBeforePatrolling(float waitTime)
+    {
+        currentPoint = null;
+        yield return new WaitForSeconds(waitTime);
+
+        if (currentPointIndex < patrolPoints.Length - 1)
+        {
+            currentPoint = patrolPoints[++currentPointIndex];
+            Debug.Log(currentPointIndex);
+        }
+        else
+        {
+            currentPoint = patrolPoints[0];
+            currentPointIndex = 0;
         }
     }
 }
